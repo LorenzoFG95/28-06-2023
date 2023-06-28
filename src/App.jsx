85ad5402891dd308a89087752d494b526c1730ba
LorenzoFG95 <lorenzofraterrigo@gmail.com>
2023-06-28 15:39:01 +0200

    feat: paragraph

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Para from "./components/Para";
 
 function App() {
   return (
     <>
       <h1 className="App_title">Titolo</h1>
+      <Para />
     </>
   );
 }
---
+++ src/components/Para/Para.jsx
@@ -0,0 +1,11 @@
+import "./index.css";
+const Para = () => {
+  return (
+    <p className="Para">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur vitae
+      nostrum possimus dolores placeat ad vero deserunt, quam aspernatur
+      voluptatibus.
+    </p>
+  );
+};
+export default Para;
