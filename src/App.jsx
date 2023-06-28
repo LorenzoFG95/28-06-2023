4d44b33f56b72961530aab31ec6268b5ebdf65a5
LorenzoFG95 <lorenzofraterrigo@gmail.com>
2023-06-28 16:23:53 +0200

    feat: add changing background color

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { useEffect } from "react";
 import "./App.css";
 import Para from "./components/Para";
 
 function App() {
+  useEffect(() => {
+    setInterval(() => {
+      document.body.style.background = `#${Math.floor(
+        Math.random() * 16777215
+      ).toString(16)}`;
+    }, 1000);
+  }, []);
+
   return (
     <>
       <h1 className="App_title">Titolo</h1>
